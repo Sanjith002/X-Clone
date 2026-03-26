80fec9f218182fa2fefbf5ada664800afc814127
Sanjith002 <sanjithkumar0204@gmail.com>
2026-03-26 19:32:24 +0530

    orgin changed

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://x-clone02.pages.dev",
     credentials: true
 }))
 app.use(express.urlencoded({
